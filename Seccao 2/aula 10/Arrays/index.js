a1d1fa77218664cd5be772a1ad5703806258063d
Willton <willtondacosta@gmail.com>
2024-10-10 22:00:55 +0200

    terminei os estudos

---
+++ Seccao 2/aula 10/Arrays/index.js
@@ -0,0 +1,21 @@
+const alunos = ['Willton', "Tomas", "Maria"];
+
+// alunos[0] = 'Eduardo'
+// alunos[3] = 'Luiza'
+
+// alunos.push("Luana")
+
+// alunos.unshift("Fernanda")
+// alunos.unshift("Fabio")
+// const removido = alunos.shift();
+// console.log(alunos)
+// console.log(removido)
+
+// alunos.push("Fernanda");
+// alunos.push("Pablo");
+
+console.log( typeof alunos)
+console.log(alunos instanceof Array)
+// console.log(alunos);
+// console.log(alunos[0]);
+// console.log(alunos[2]);
