1fbc8d3ee97d0cee4a28f46638c1e6bd9080f535
Willton <willtondacosta@gmail.com>
2024-10-23 16:09:50 +0200

    terminei por agora

---
+++ Seccao 9/aula 75/inport_export/src/index.js
@@ -0,0 +1,7 @@
+const nome ='Willas';
+const sobrenome = 'Xavier';
+const idade = 24;
+
+function soma(x,y){
+    return x + y;
+}
---
+++ Seccao 9/modelo_webpack/public/assets/js/bundle.js
@@ -0,0 +1,2 @@
+console.log("EIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII");
+//# sourceMappingURL=bundle.js.map
