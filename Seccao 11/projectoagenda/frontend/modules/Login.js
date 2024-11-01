72738b4a6c172ecff382efe28009a9dc9df6600a
Willton <willtondacosta@gmail.com>
2024-11-01 19:04:49 +0200

    terminei por hoje

--- Seccao 11/projectoagenda/frontend/modules/Login.js
+++ Seccao 11/projectoagenda/frontend/modules/Login.js
@@ -1,3 +1,5 @@
+import validator from 'validator';
+
 export default class Login{
     constructor(formClass){
         this.form = document.querySelector(formClass);
@@ -19,7 +21,18 @@ export default class Login{
         const el = e.target;
         const emailInput = el.querySelector('input[name="email"]');
         const passwordInput = el.querySelector('input[name="password"]');
+        let error = false;
 
-        console.log(emailInput.value, passwordInput.value)
+        if(!validator.isEmail(emailInput.value)){
+            alert("E-mail invalido");
+            error = true;
+        }
+
+        if(passwordInput.value.length < 3 || passwordInput.value.length > 50){
+            alert("Senha precisa ter entre 3 e 50 caracteres");
+            error = true;
+        }
+        
+        if(!error) el.submit();
     }
 }
