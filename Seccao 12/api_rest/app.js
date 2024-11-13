e16754cd06c4fe0173573cd3f4bc6c1781d7289a
Willton Xavier <willtondacosta@gmail.com>
2024-11-13 10:22:12 +0200

    terminei por agora

--- Seccao 12/api_rest/app.js
+++ Seccao 12/api_rest/app.js
@@ -5,6 +5,7 @@ import './src/database';
 import express from 'express';
 import homeRoutes from './src/routes/homeRoutes';
 import userRoutes from './src/routes/userRoutes';
+import tokensRoutes from './src/routes/tokenRoutes';
 
 class App {
   constructor() {
@@ -21,6 +22,7 @@ class App {
   routes() {
     this.app.use('/', homeRoutes);
     this.app.use('/users/', userRoutes);
+    this.app.use('/tokens/', tokensRoutes);
   }
 }
 
