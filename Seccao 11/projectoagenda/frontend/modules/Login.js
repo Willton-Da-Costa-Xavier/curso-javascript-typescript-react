ed26296be424d6adf1188ea7b9155061a3fa00e9
Willton Xavier <willtondacosta@gmail.com>
2025-03-15 18:54:19 +0200

    nao sei

--- Seccao 11/projectoagenda/frontend/modules/Login.js
+++ Seccao 11/projectoagenda/frontend/modules/Login.js
@@ -35,4 +35,6 @@ export default class Login{
         
         if(!error) el.submit();
     }
+
+    
 }
--- Seccao 12/api_rest/src/controllers/AlunoController.js
+++ Seccao 12/api_rest/src/controllers/AlunoController.js
@@ -4,6 +4,44 @@ class AlunoController {
     const alunos = await Aluno.findAll();
     res.json(alunos);
   }
+
+  async store(req, res) {
+
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      }
+
+      const aluno = await Aluno.findByPk(id);
+
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno nao existe'],
+        });
+      }
+
+      return res.json(aluno);
+    } catch (error) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+
+  }
+
+  async delete(req, res) {
+
+  }
 }
 
 export default new AlunoController();
