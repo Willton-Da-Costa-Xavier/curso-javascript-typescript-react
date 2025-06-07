ceb9a8db2d29d50bfff7616e2e391dc6e91cbc02
Willton Xavier <willtondacosta@gmail.com>
2025-06-07 11:55:24 +0200

    terminei por agora

---
+++ Seccao 13/REACT/reactbase/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    requireConfigFile: false,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': 2,
+    'react/jsx-filename-extension': 0,
+    'import/prefer-default-export': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 'off',
+    'no-undef': 'off',
+  },
+};
---
+++ Seccao 13/REACT/reactbase/src/pages/Login/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Title, Paragrafo } from './styled';
+
+export default function Login() {
+  return (
+    <React.Fragment>
+      <Title>
+        Login
+        <small>Oie</small>
+      </Title>
+      <Paragrafo>Loren6</Paragrafo>
+    </React.Fragment>
+  );
+}
