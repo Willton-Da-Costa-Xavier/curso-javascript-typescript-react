4372da5ce2473d813c8ff918d9cbc2e819337512
Jwise Devs <willtondacosta@gmail.com>
2026-05-12 21:34:02 -0700

    terminei por hoje

---
+++ Seccao 17/expressoes_regulares/aula2.js
@@ -0,0 +1,9 @@
+const { texto } = require('./base');
+const regExp1 = /Joao|Maria/gi;
+
+// console.log(texto);
+// console.log(texto.match(regExp1));
+// console.log(texto.replace(/(Joao|Maria)/gi, '<b>$1</b>'))
+// console.log(texto.replace(/(Joao|Maria)/gi, function(input){
+//     return ' ############### ' + input.toUpperCase() + ' ############### ';
+// }))
